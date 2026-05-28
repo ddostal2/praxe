916be2cc93122c42fd6def4cacc59ba338df1f83
jfigielst <j.figiel.st@spseiostrava.cz>
2026-05-28 12:19:43 +0200

    css pro OrderConfirmationPage

--- projekt/client/src/pages/OrderConfirmationPage.jsx
+++ projekt/client/src/pages/OrderConfirmationPage.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/PageShared.css';
-import './OrderConfirmationPage.css';
+import '../styles/OrderConfirmationPage.css';
 
 const formatDateTime = (isoString) => {
   if (!isoString) return '-';
