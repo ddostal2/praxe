345b38d931bbab1402a86b29bd291faa39e076e8
jfigielst <j.figiel.st@spseiostrava.cz>
2026-05-29 09:28:51 +0200

    repo

--- Richard a Honza/src/App.jsx
+++ Richard a Honza/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function App() {
   return (
     <div style={{
@@ -6,25 +8,12 @@ function App() {
       justifyContent: 'center',
       alignItems: 'center',
       minHeight: '100vh',
-      textAlign: 'center',
-      padding: '20px'
+      fontFamily: 'sans-serif',
+      color: '#333'
     }}>
-      <h1 style={{
-        fontSize: '2.5rem',
-        fontWeight: '300',
-        color: '#1a1a1a',
-        marginBottom: '10px'
-      }}>
-        Čistá stránka
-      </h1>
-      <p style={{
-        color: '#666',
-        fontSize: '1.1rem'
-      }}>
-        Můžete začít psát svůj kód v <code>src/App.jsx</code>.
-      </p>
+      <h1>stránka</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Richard a Honza/src/modules/Mars/MarsModule.jsx
@@ -0,0 +1,1 @@
+// MarsModule Template
